38b643aa9c9dba367357e6c65b22553d11dd3bb3
Nibedan <nibedanpati@gmail.com>
2024-10-29 12:58:57 +0530

    CORS

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ import './config/dotenv.js';
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://manipal-hackathon.vercel.app', 
     credentials: true,                
   }));
 
